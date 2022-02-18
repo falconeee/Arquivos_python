568000dc463694c0dff921033183749c92613a0b
falconeee <54122603+falconeee@users.noreply.github.com>
2022-02-18 08:21:18 -0300

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -7,6 +7,5 @@ level_battery = 25     #Nível de bateria para o sistema desligar
 while True: #Inserir código dentro no while do programa
     if api.get_battery_level() <= level_battery:  #Se o nível de bateria cai abaixo de um certo valor o sistema habilita o evento de falta de energia,logo só desligará se estiver sem energia da rede e abaixo do nível especificado.                                                  
         api.set_power_outage_event_status(1)      #Evento habilitado e inicia o processo de desligamento
-        print('SHUTDOWN')
 
         
